1cf47fde8ac970cb9e4f6e72dda13a5296a02a30
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-09-06 09:55:52 +0200

    pull current reps display outside button, block any scroll movement mobile, and desktop

--- app/page.tsx
+++ app/page.tsx
@@ -9,31 +9,39 @@ export default function Home() {
   const [currentReps, setCurrentReps] = useState(0)
   const [repeatsCount, setRepeatsCount] = useState(0)
   const [totalReps, setTotalReps] = useState(0)
-  const [repsInSet, setRepsInSet] = useState(10)
+  const [repsInSet, setRepsInSet] = useState(8)
 
   const handleCount = () => {
-    setCurrentReps((prev) => prev + 1)
+    setCurrentReps(currentReps + 1)
   }
+
+  useEffect(() => {
+    document.addEventListener(
+      'touchmove',
+      (event) => {
+        event.preventDefault()
+      },
+      { passive: false }
+    )
+  }, [])
 
   useEffect(() => {
     currentReps !== 0 && setTotalReps(totalReps + 1)
     //if currentReps get the total amount of reps in set
     if (currentReps >= repsInSet) {
-      setRepeatsCount((prev) => prev + 1)
+      setRepeatsCount(repeatsCount + 1)
       setCurrentReps(0)
     }
   }, [currentReps])
 
   return (
-    <main className="flex items-center justify-around h-dvh flex-col">
+    <main className="flex items-center justify-around absolute w-full h-full flex-col">
       <RepsButton
         repsInSet={repsInSet}
         setRepsInSet={setRepsInSet}
       />
-      <CountButton
-        count={currentReps}
-        handleCount={handleCount}
-      />
+      <h1 className="text-8xl">{currentReps}</h1>
+      <CountButton handleCount={handleCount} />
       <div className="flex items-center justify-around w-full">
         <SetsCounter repeatsCount={repeatsCount} />
         <TotalReps totalReps={totalReps} />
--- components/count-button.tsx
+++ components/count-button.tsx
@@ -1,19 +1,15 @@
 import { MouseEventHandler } from 'react'
 
 const CountButton = ({
-  count,
   handleCount,
 }: {
-  count: number
   handleCount: MouseEventHandler<HTMLButtonElement>
 }) => {
   return (
     <button
       onClick={handleCount}
       className="bg-green-500 rounded-full size-48 text-6xl outline outline-2 outline-offset-[6px] hover:outline-offset-2 outline-green-500"
-    >
-      {count}
-    </button>
+    ></button>
   )
 }
 
